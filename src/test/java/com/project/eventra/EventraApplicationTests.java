cc33ff382c18a44d37aa8f7c0ecb52305bf32c3d
Rakshit Upadhyay <rakshit.upadhyay@hotwaxsystems.com>
2025-12-21 23:58:01 +0530

    Initial commit

---
+++ src/test/java/com/project/eventra/EventraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
