b92dfe9ea3a8f174760a89fccaca89a054ae5e90
rakshit-upadhyay214 <rakshitupadhyay.2004@gmail.com>
2026-01-01 23:48:36 +0530

    Added QrCode entity and updated relation to ticket entity

--- src/main/java/com/project/eventra/domain/QrCodeStatusEnum.java
+++ src/main/java/com/project/eventra/domain/QrCodeStatusEnum.java
@@ -1,6 +1,6 @@
 package com.project.eventra.domain;
 
-public enum QRCodeStatusEnum {
+public enum QrCodeStatusEnum {
     ACTIVE, // QR code can be used
     EXPIRED // QR code has been invalidated
 }
